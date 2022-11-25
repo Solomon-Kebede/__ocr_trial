3149caed00ba1c4d8665a12cee2b258dc0347a1a
Solomon Kebede <solomonaau188@gmail.com>
2022-11-25 21:06:33 +0000

    Add async main function

--- main2.py
+++ main2.py
@@ -61,3 +61,85 @@ async def post(session, url, _headers, _data=None, request_type = None):
 				logging.error(f'Status Code: {response.status}')
 				return None
 
+async def main():
+	async with aiohttp.ClientSession() as session:
+		results_endpoint1 = (
+			await asyncio.gather(
+			*[
+			post(session, endpoint_1,
+				header('upload_url'),
+				request_type="headers-only") for image_path in image_paths]
+		)
+			)
+		filepath = get_path(image_paths)
+		endpoint_2_awaitables = []
+		endpoint_3_awaitables = []
+		for r in results_endpoint1:
+			upload_location = r.get('X-Goog-Upload-URL')
+			logging.info(f"[1.0] {upload_location}\n")
+			# Read Image
+			image_data = read_image(next(filepath))
+			# Upload Image
+			# res_2 = requests.post(upload_location, headers = header('upload_image'), data = image_data)
+			'''[TODO]
+			All the images have to be loaded into
+			memory [Improve design]
+			'''
+			endpoint_2_awaitables.append(
+			post(session, upload_location,
+				header('upload_image'),
+				_data=image_data,
+				request_type="text-only")
+			)
+			
+		results_endpoint2 =	await asyncio.gather(*endpoint_2_awaitables)
+		# print(results_endpoint2)
+		for r in results_endpoint2:
+			res_2_json = json.loads(r.split('\n')[1])
+			# print('=' * 30)
+			logging.info(f"[2.0] {res_2_json}\n")
+			# Get Google Lens Location
+			gl_url = f'{base_url}{res_2_json["url"]}'
+
+			# Get Google Photo Link
+			gp_url_encoded = res_2_json["payload"]
+			gp_url_decoded = base64.b64decode(gp_url_encoded)
+			gp_url = f"https://{gp_url_decoded.split(b'https://')[1].split(b'(')[0].decode('utf-8')}"
+
+			logging.info(f"[3.0] {gp_url}\n")
+			# Get OCR data
+			results_url = url4batchexecute()
+			data = data4batchexecute(gp_url)
+			endpoint_3_awaitables.append(
+				post(session, results_url,
+					header(),
+					_data=data,
+					request_type="text-only")
+				)
+		results_endpoint3 =	await asyncio.gather(*endpoint_3_awaitables)
+		# print(results_endpoint3)
+		for r in results_endpoint3:
+			# Parsing for the result
+			result_1 = r.split('\n')[3]
+			result_2 = json.loads(result_1)[0][2]
+			result_3 = repr(result_2)
+
+			c = 0
+			all_lists = []
+
+			for i in result_2.split("["):
+				for j in i.split("]"):
+					if j != '':
+						potential_list = (f"{j}")
+						all_lists.append(potential_list)
+						c+=1
+
+			ocr_result = all_lists[-45]
+			ocr_results = json.loads(f"[{ocr_result}]")
+			logging.info(f"[4.0] {ocr_results}\n")
+
+			print("Final Result:")
+			for ocr_item in ocr_results:
+				print(f"{ocr_item}")
+			print()
+
